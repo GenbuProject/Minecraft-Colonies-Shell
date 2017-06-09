a76d92fbf82fbbdb5639f48f09a75f6c170770b4
Genbu Hase <hasegenbu@gmail.com>
2017-06-09 19:07:28 +0900

    処理をMain.jsに統合。 ログイン処理追加。

---
+++ Main.js
@@ -0,0 +1,35 @@
+let GBase = null,
+	accountInfo = {};
+	
+DOM.importAPI("https://genbuproject.github.io/Programs/Sync Helper/Sync Helper v1.1.js", function () {
+	GBase = new GoogleAPI({
+		ID: "107562519312-bbaa0fedfsd8nsob29fkbo3asu5eulnt.apps.googleusercontent.com",
+		Key: atob("UlEzVjh0M1VjQXI2ODNLNDBNdTNadlhr"),
+		Url: "https://genbuproject.github.io/Minecraft-Colonies-Shell/" //"http://localhost:8001/"
+	});
+
+	(function () {
+		let LoopCount = 0;
+
+		let Looper = setInterval(function () {
+			if (GBase.hasLogined()) {
+				clearInterval(Looper);
+
+				accountInfo = GBase.getUserInfo();
+				DOM("#AccountName").textContent = accountInfo.displayName;
+			}
+		}, 1000);
+	})();
+});
+
+window.addEventListener("DOMContentLoaded", function () {
+	DOM('@*[UUID="Handler__SignIn"]').forEach(function (Elem, Index, Parent) {
+		Elem.addEventListener("click", function () {
+			if (!navigator.isMobile()) {
+				GBase.loginOnDialog(GoogleAPI.SCOPE.PLUS);
+			} else {
+				GBase.login(GoogleAPI.SCOPE.PLUS);
+			}
+		});
+	});
+});
